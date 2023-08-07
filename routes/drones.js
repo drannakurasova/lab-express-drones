a94e027dcbb4ecfd057f307abe89a9a47a0bd987
dcritoballl <d.cristobalgallego@gmail.com>
2023-08-07 19:01:22 +0200

    tercera iteracion completada, desde este punto Dani no colabora

--- routes/drones.js
+++ routes/drones.js
@@ -25,12 +25,50 @@ router.get('/drones', (req, res, next) => {
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
   // ... your code here
+  res.render("drones/create-form.hbs")
 });
 
-router.post('/drones/create', (req, res, next) => {
+
+
+
+
+router.post("/drones/create", (req, res, next) => {
   // Iteration #3: Add a new drone
   // ... your code here
+  console.log(req.body)
+  Drone.create({
+    name: req.body.name,
+    propellers: req.body.propellers,
+    maxSpeed: req.body.maxSpeed,
+  })
+  .then(()=>{
+  res.redirect("/drones")
+
+  })
+  .catch((error)=>{
+    next(error)
+  })
+
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
