c25b3d7026ff735564cc3ce62bacca445c6bfdf2
dcritoballl <d.cristobalgallego@gmail.com>
2023-08-07 17:37:03 +0200

    primera iteracion completada

--- routes/drones.js
+++ routes/drones.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const Drone =require("../models/Drone.model.js")
+
 // require the Drone model here
 
 router.get('/drones', (req, res, next) => {
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,32 @@
 // Iteration #1
+
+const drones = [
+  { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+
+  { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+
+  { name: "Courier 3000i", propellers: 6, maxSpeed: 18 },
+];
+
+const Drone = require("../models/Drone.model.js");
+
+//Conexion mongoose
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/lab-express-drones")
+
+// const MONGO_URI =
+//   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/lab-express-drones";
+
+Drone.create(drones)
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+
+ //Paso "opcional" de la iteracion 1, aprender a cerrar una conexion
+  mongoose.disconnect("mongodb://127.0.0.1:27017/lab-express-drones")
+
+module.exports = Drone;
